dd861b19d538dd6ae63566985a7f93cabc990308
Bisheng Liu <bisheng.liu@abcam.com>
2022-01-22 09:26:15 +0100

    feat:typeorm postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmConfigService } from './database/typeorm.config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/typeorm.config.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private _configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      // host: this._configService.get(POSTGRES_HOST) || 'postgres-db',
+      host: 'localhost', // for local dev
+      port: +this._configService.get<number>('POSTGRES_PORT') || 5432,
+      username: this._configService.get('DATABASE_USER') || 'postgres',
+      password: this._configService.get('DATABASE_PASSWORD') || 'postgres',
+      database: this._configService.get('DATABASE_NAME') || 'catalog',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      migrationsRun: true,
+      synchronize: true,
+      logging: true,
+      migrationsTableName: 'migrations',
+      migrations: [__dirname + '/migrations/**/*.ts'],
+      autoLoadEntities: true,
+      cli: {
+        migrationsDir: 'src/migrations',
+      },
+    };
+  }
+}
