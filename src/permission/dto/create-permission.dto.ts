1d1ffc071558a3723e020fa99ef0bd8cdec51531
Bisheng Liu <bisheng.liu@abcam.com>
2022-01-23 09:34:30 +0100

    feat:update dto's

--- src/permission/dto/create-permission.dto.ts
+++ src/permission/dto/create-permission.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePermissionDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsUUID } from 'class-validator';
+
+export class CreatePermissionDto {
+  @ApiProperty({ type: String })
+  @IsUUID(4)
+  userId: string;
+
+  @ApiProperty({ type: String })
+  @IsUUID(4)
+  serviceId: string;
+}
