a612609719d6a8b61dd5493209c81058b4fa8690
Bisheng Liu <bisheng.liu@abcam.com>
2022-01-30 23:00:16 +0100

    feat:get repo fron testingmodule

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
 import { User } from '../src/auth/entities/user.entity';
 import { AuthModule } from '../src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { UserRepository } from './../src/auth/repository/user.repository';
+import { AuthService } from './../src/auth/services/auth.service';
+import { JwtStrategy } from './../src/auth/strategies/jwt-strategy';
+import { getRepository, Repository } from 'typeorm';
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
+  let userRepository;
 
   beforeEach(async () => {
+    jest.resetModules();
+    process.env.JWT_SECRET = 'JWT_SECRET';
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
         AuthModule,
@@ -18,16 +26,27 @@ describe('AuthController (e2e)', () => {
           logging: true,
           synchronize: true,
         }),
+        TypeOrmModule.forFeature([UserRepository]),
+        JwtModule.registerAsync({
+          useFactory: async () => ({
+            secret: 'JWT_SECRET',
+            signOptions: {
+              expiresIn: 3600,
+            },
+          }),
+        }),
       ],
+      providers: [AuthService, JwtStrategy],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    userRepository = moduleFixture.get<UserRepository>(UserRepository);
   });
 
-  // afterAll(async () => {
-  //   await app.close();
-  // });
+  afterEach(async () => {
+    await userRepository.query('DELETE FROM user');
+  });
 
   describe('health endpoint', () => {
     it('/', () => {});
